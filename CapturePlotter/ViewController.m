644be33b47106c4230941ab76c71528682f01972
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-23 13:18:27 -0700

    Added AIHTTPRequest

--- CapturePlotter/ViewController.m
+++ CapturePlotter/ViewController.m
@@ -8,11 +8,27 @@
 
 #import "ViewController.h"
 
+#define METERS_PER_MILE 1609.344
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+-(void)viewWillAppear:(BOOL)animated {
+    // 1
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 39.281516;
+    zoomLocation.longitude= -76.580806;
+    
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    //3
+    [_mapView setRegion:viewRegion animated:YES];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
